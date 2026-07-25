c3b498cf22d2d0f13fa4ecd7d258b2d73ebdc36f
deku <liberty3toshi@gmail.com>
2026-07-25 18:07:46 +0900

    test: rewrite tobacco_types request spec with factories

--- spec/requests/v1/tobacco_types_spec.rb
+++ spec/requests/v1/tobacco_types_spec.rb
@@ -3,18 +3,16 @@ require 'rails_helper'
 RSpec.describe "V1::TobaccoTypes", type: :request do
   describe "GET /v1/tobacco_types" do
     it "returns tobacco types sorted by display_order with only allowed fields" do
-
-      TobaccoType.create!(name: "電子タバコ", icon: "electronic cigarette", display_order: 2)
-      TobaccoType.create!(name: "紙タバコ",   icon: "cigarette",            display_order: 1)
+      # 並び替えを検証するため、あえて display_order の降順で作成する
+      create(:tobacco_type, :electronic)
+      create(:tobacco_type, :paper)
 
       get "/v1/tobacco_types"
 
       expect(response).to have_http_status(:ok)
-
       json = JSON.parse(response.body)
 
       expect(json.size).to eq 2
-
       expect(json.map { |t| t["name"] }).to eq ["紙タバコ", "電子タバコ"]
       expect(json.map { |t| t["display_order"] }).to eq [1, 2]
 
